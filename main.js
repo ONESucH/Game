438cef29d3e543c7d1a7c8be7c3de92e8138f859
Bogdanov Ilya <onesuch07@gmail.com>
2017-11-14 14:06:19 +0300

    Добавили меню

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 'use strict';
 
-start();
+/* Загружаем меню */
+// loadingMenu();
+//
+// function loadingMenu() {
+//     var content = document.getElementsByTagName('body')[0],
+//         sheath = document.createElement('div'),
+//         menu = document.createElement('div'),
+//         title = document.createElement('div'),
+//         play = document.createElement('div'),
+//         playInformation = document.createElement('div');
+//
+//     content.innerHTML = ''; // чистим контайнер
+//     content.className = 'content'; // content
+//     sheath.className = 'sheath'; // оболочка
+//     menu.className = 'menu'; // меню
+//
+//     /* Добавляем содержимое в меню */
+//     title.innerHTML = 'Hello Snake';
+//     play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+//     playInformation.innerHTML = 'Нажми, чтобы начать игру';
+//
+//     /* Отслеживаем запуск игры */
+//     play.onclick = function () {
+//         start();
+//     };
+//
+//     menu.append(title, play, playInformation); // меню
+//     sheath.appendChild(menu);
+//     content.appendChild(sheath);
+// }
 
-function start() {
+start(); // tmp 
+
+function start() { // запускаем игру
     var content = document.getElementsByTagName('body')[0],
         div = document.createElement('div'),
-        timer = 1, // скорость анимации
+        timer = 1, // скорость анимации передвижения
         counterVertical = 0,
         counterGorizontal = 0;
 
+    content.innerHTML = '';
     div.className = 'draw-circle';
 
     setInterval(function () {
@@ -22,13 +54,13 @@ function start() {
         if (counterVertical >= widthWindow + sizeCircle) { // Положительное значение поля по горизонтали "Право"
             counterVertical = 0;
         }
-        if (counterVertical <= 0 - sizeCircle ) { // Отрицательное значение поля по горизонтали "Лево"
+        if (counterVertical <= 0 - sizeCircle) { // Отрицательное значение поля по горизонтали "Лево"
             counterVertical = widthWindow;
         }
         if (counterGorizontal >= heightWindow + sizeCircle) { // Положительное значение поля по вертикали "Низ"
             counterGorizontal = 0;
         }
-        if (counterGorizontal <= 0 - sizeCircle ) { // Отрицательное значение поля по вертикали "Верх"
+        if (counterGorizontal <= 0 - sizeCircle) { // Отрицательное значение поля по вертикали "Верх"
             counterGorizontal = heightWindow;
         }
 
