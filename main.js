ed8787a39494be4a53d3af7956a1aa4e25886e2c
Bogdanov Ilya <onesuch07@gmail.com>
2017-11-14 15:51:30 +0300

    Осталось добавить event событие для отслежки столкновения

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
 'use strict';
 
 /* Загружаем меню */
-// loadingMenu();
-//
-// function loadingMenu() {
-//     var content = document.getElementsByTagName('body')[0],
-//         sheath = document.createElement('div'),
-//         menu = document.createElement('div'),
-//         title = document.createElement('div'),
-//         play = document.createElement('div'),
-//         playInformation = document.createElement('div');
-//
-//     content.innerHTML = ''; // чистим контайнер
-//     content.className = 'content'; // content
-//     sheath.className = 'sheath'; // оболочка
-//     menu.className = 'menu'; // меню
-//
-//     /* Добавляем содержимое в меню */
-//     title.innerHTML = 'Hello Snake';
-//     play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-//     playInformation.innerHTML = 'Нажми, чтобы начать игру';
-//
-//     /* Отслеживаем запуск игры */
-//     play.onclick = function () {
-//         start();
-//     };
-//
-//     menu.append(title, play, playInformation); // меню
-//     sheath.appendChild(menu);
-//     content.appendChild(sheath);
-// }
+loadingMenu();
 
-start(); // tmp 
+function loadingMenu() {
+    var content = document.getElementsByTagName('body')[0],
+        sheath = document.createElement('div'),
+        menu = document.createElement('div'),
+        title = document.createElement('div'),
+        play = document.createElement('div'),
+        playInformation = document.createElement('div');
+
+    content.innerHTML = ''; // чистим контайнер
+    content.className = 'content'; // content
+    sheath.className = 'sheath'; // оболочка
+    menu.className = 'menu'; // меню
+
+    /* Добавляем содержимое в меню */
+    title.innerHTML = 'Beware of enemies';
+    play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    playInformation.innerHTML = 'Нажми, чтобы начать игру';
+
+    /* Отслеживаем запуск игры */
+    play.onclick = function () {
+        start();
+    };
+
+    menu.append(title, play, playInformation); // меню
+    sheath.appendChild(menu);
+    content.appendChild(sheath);
+}
 
 function start() { // запускаем игру
     var content = document.getElementsByTagName('body')[0],
-        div = document.createElement('div'),
-        timer = 1, // скорость анимации передвижения
+        userCircle = document.createElement('div'),
+        randomPostionCircle = Math.floor(Math.random() * (window.innerHeight - 25) + 1), // рандомная позиция user
         counterVertical = 0,
-        counterGorizontal = 0;
+        level = 1 , // уровень, чем больше тем сложнее
+        counterGorizontal = randomPostionCircle;
 
     content.innerHTML = '';
-    div.className = 'draw-circle';
+    userCircle.className = 'draw-circle';
 
     setInterval(function () {
+
+        /* Движение пользовательского шара */
         var widthWindow = window.innerWidth,
             heightWindow = window.innerHeight,
             sizeCircle = 25;
 
-        div.style.marginLeft = counterVertical + 'px';
-        div.style.marginTop = counterGorizontal + 'px';
+        userCircle.style.marginLeft = counterVertical + 'px';
+        userCircle.style.marginTop = counterGorizontal + 'px';
+
+        counterVertical++;
 
         if (counterVertical >= widthWindow + sizeCircle) { // Положительное значение поля по горизонтали "Право"
             counterVertical = 0;
@@ -65,9 +68,8 @@ function start() { // запускаем игру
         }
 
         window.onkeydown = function (item) {
-            console.log(item);
             if (item.key === 'ArrowLeft') { // нажатие на лево
-                counterVertical -= 20;
+                counterVertical -= 30;
             }
             if (item.key === 'ArrowRight') { // нажатие на лево
                 counterVertical += 20;
@@ -78,8 +80,35 @@ function start() { // запускаем игру
             if (item.key === 'ArrowDown') { // нажатие на лево
                 counterGorizontal += 20;
             }
-        }
-    }, timer);
+        };
 
-    content.appendChild(div);
+    }, 10);
+
+    content.appendChild(userCircle);
+    moveBots();
+    setInterval(function () {
+        moveBots();
+    }, level * 1000);
+}
+
+/* Создание, движение ботов */
+function moveBots() {
+    var content = document.getElementsByTagName('body')[0],
+        counterBots = window.innerWidth, // Скорость ботов
+        bot = document.createElement('div'),
+        randomPostionCrazyCircle = Math.floor(Math.random() * (window.innerHeight) - 5); // рандомная позиция врага
+
+    bot.style.marginTop = randomPostionCrazyCircle + 'px';
+
+    setInterval(function () {
+        bot.style.marginLeft = counterBots + 20 + 'px';
+        bot.className = 'draw-circle-crazy';
+        content.appendChild(bot);
+
+        if (counterBots <= -25) {
+            content.removeChild(bot);
+        }
+
+        counterBots--;
+    }, 10);
 }
