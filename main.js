f0ff1aaea2d8dabe7ca909fe5722ee53cbe442df
Bogdanov Ilya <bogdanov@comepay.ru>
2018-03-16 15:03:09 +0300

    fix game

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 'use strict';
-var dataPosition = {};
+var content = document.getElementsByTagName('body')[0],
+    dataPosition = {};
+
 /* Загружаем меню */
 loadingMenu();
 
+/* Стартовое меню */
 function loadingMenu() {
-    var content = document.getElementsByTagName('body')[0],
-        sheath = document.createElement('div'),
+    var sheath = document.createElement('div'),
         menu = document.createElement('div'),
         title = document.createElement('div'),
         play = document.createElement('div'),
@@ -17,9 +19,9 @@ function loadingMenu() {
     menu.className = 'menu'; // меню
 
     /* Добавляем содержимое в меню */
-    title.innerHTML = 'Beware of enemies';
-    play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-    playInformation.innerHTML = 'Нажми, чтобы начать игру';
+    title.innerHTML = 'Beware of enemies'; // Название игры , шрифты работают с англ. яз.
+    play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>'; // кнопка запуска
+    playInformation.innerHTML = 'Press, play'; // информационное поле
 
     /* Отслеживаем запуск игры */
     play.onclick = function () {
@@ -31,21 +33,21 @@ function loadingMenu() {
     content.appendChild(sheath);
 }
 
-function start() { // запускаем игру
-    var content = document.getElementsByTagName('body')[0],
-        userCircle = document.createElement('div'), // пользовательский круг
-        randomPositionCircle = Math.floor(Math.random() * (window.innerHeight - 25) + 1), // рандомная позиция user
-        counterVertical = 0,
+/* Запускаем игру */
+function start() {
+    var userCircle = document.createElement('div'), // пользовательский круг
+        randomPositionCircle = Math.floor(Math.random() * (window.innerHeight - 25) + 1), // рандомная позиция пользователя
+        counterVertical = 0, // нулевое значение по горизонтали
+        counterGorizontal = randomPositionCircle, // положение нашего круга вертикали
         level = 1, // уровень, чем больше тем сложнее
-        counterGorizontal = randomPositionCircle,
-        sizeCircle = 25;
+        sizeCircle = 25; // размер нашего круга
 
-    content.innerHTML = '';
+    content.innerHTML = ''; // очищаем всё что было внутри контейнера
     userCircle.className = 'draw-circle';
 
     setInterval(function () {
-        var X = userCircle.getBoundingClientRect().left,
-            Y = userCircle.getBoundingClientRect().top;
+        var X = userCircle.getBoundingClientRect().left, // координаты по Х
+            Y = userCircle.getBoundingClientRect().top; // координаты по Y
 
         dataPosition.userPosition = {
             positionX: X,
@@ -54,97 +56,94 @@ function start() { // запускаем игру
 
     }, 200);
 
+    /* Движение пользовательского шара */
     setInterval(function () {
-        /* Движение пользовательского шара */
-        var widthWindow = window.innerWidth,
-            heightWindow = window.innerHeight;
+        var widthWindow = window.innerWidth, // получаем значение ширины окна браузера
+            heightWindow = window.innerHeight; // получаем значение высоты окна браузера
+            
+        userCircle.style.marginLeft = counterVertical + 'px'; // положение пользователя по Х и Y
+        userCircle.style.marginTop = counterGorizontal + 'px'; // положение пользователя по Х и Y
 
-        userCircle.style.marginLeft = counterVertical + 'px';
-        userCircle.style.marginTop = counterGorizontal + 'px';
-
-        counterVertical++;
+        counterVertical++; // движение круга пользователя
 
         if (counterVertical >= widthWindow + sizeCircle) { // Положительное значение поля по горизонтали "Право"
-            counterVertical = 0;
+            counterVertical = 0; // возращаем круг в нужную позицию, если он вылетел за блок
         }
         if (counterVertical <= 0 - sizeCircle) { // Отрицательное значение поля по горизонтали "Лево"
-            counterVertical = widthWindow;
+            counterVertical = widthWindow; // возращаем круг в нужную позицию, если он вылетел за блок
         }
         if (counterGorizontal >= heightWindow + sizeCircle) { // Положительное значение поля по вертикали "Низ"
-            counterGorizontal = 0;
+            counterGorizontal = 0; // возращаем круг в нужную позицию, если он вылетел за блок
         }
         if (counterGorizontal <= 0 - sizeCircle) { // Отрицательное значение поля по вертикали "Верх"
-            counterGorizontal = heightWindow;
+            counterGorizontal = heightWindow; // возращаем круг в нужную позицию, если он вылетел за блок
         }
 
+        /* Следим за нажатием на стрелочки */
         window.onkeydown = function (item) {
             if (item.key === 'ArrowLeft') { // нажатие на лево
                 counterVertical -= 30;
             }
-            if (item.key === 'ArrowRight') { // нажатие на лево
+            if (item.key === 'ArrowRight') { // нажатие на право
                 counterVertical += 20;
             }
-            if (item.key === 'ArrowUp') { // нажатие на лево
+            if (item.key === 'ArrowUp') { // нажатие на верх
                 counterGorizontal -= 20;
             }
-            if (item.key === 'ArrowDown') { // нажатие на лево
+            if (item.key === 'ArrowDown') { // нажатие на низ
                 counterGorizontal += 20;
             }
         };
 
-    }, 10);
+    }, 50); // скорость пользовательского шарика
 
     content.appendChild(userCircle);
+    /* Создание, движение ботов */
     moveBots();
     /* Интервал создания ботов */
     setInterval(function () {
         moveBots();
     }, level * 1000);
+    /* Ловим позиции бота */
     targetCircle(sizeCircle);
 }
 
 /* Создание, движение ботов */
 function moveBots() {
-    var content = document.getElementsByTagName('body')[0],
-        counterBots = window.innerWidth, // Скорость ботов
+    var counterBots = window.innerWidth, // Скорость ботов
         bot = document.createElement('div'), // круг бота
         randomPostionCrazyCircle = Math.floor(Math.random() * (window.innerHeight) - 5); // рандомная позиция врага
 
-    bot.style.marginTop = randomPostionCrazyCircle + 'px';
+    bot.style.marginTop = randomPostionCrazyCircle + 'px'; // изначальная позиция круга по вертикали
 
     setInterval(function () {
-        var X = bot.getBoundingClientRect().left,
-            Y = bot.getBoundingClientRect().top;
+        var X = bot.getBoundingClientRect().left, // кооржинаты по Х
+            Y = bot.getBoundingClientRect().top; // кооржинаты по Y
 
-        dataPosition.botPosition = {
+        dataPosition.botPosition = { // записываем координаты в объект
             positionX: X,
             positionY: Y
         };
 
-        bot.style.marginLeft = counterBots + 20 + 'px';
+        bot.style.marginLeft = counterBots + 20 + 'px'; // Движение ботов
         bot.className = 'draw-circle-crazy';
-        content.appendChild(bot);
+        content.appendChild(bot); // вставляем бота в наш контайнер
 
         if (counterBots <= -25) {
-            content.removeChild(bot);
+            content.removeChild(bot); // Очищаем пройденный(за контайнер) круг
         }
 
-        counterBots--;
+        bot.parentNode.removeChild(bot); // удаляем по id тег
+        counterBots--; // движение бота
     }, 10);
-
-    /* Ловим позицию бота */
 }
 
 /* Ловим позицию ботов и юзера */
-function targetCircle(dataSizeCircle) {
+function targetCircle() {
+
     setInterval(function () {
-        if (dataPosition.userPosition.positionX === dataPosition.botPosition.positionX &&
-            dataPosition.userPosition.positionY === dataPosition.botPosition.positionY &&
-            dataPosition.userPosition.positionX + dataSizeCircle === dataPosition.botPosition.positionX + dataSizeCircle &&
-            dataPosition.userPosition.positionY + dataSizeCircle === dataPosition.botPosition.positionY + dataSizeCircle &&
-            dataPosition.userPosition.positionX - dataSizeCircle === dataPosition.botPosition.positionX - dataSizeCircle &&
-            dataPosition.userPosition.positionY - dataSizeCircle === dataPosition.botPosition.positionY - dataSizeCircle) {
-            alert('Сработала коллизия');
-        }
-    }, 200);
+        console.log('bot', dataPosition.botPosition);
+        console.log('user', dataPosition.userPosition);
+    }, 1000);
+
 }
